3a0d0af11a7665250d4930d20684ac6a7f2d78d6
diablo-legend <alexander553278@gmail.com>
2025-01-23 20:57:19 +0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,14 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Устанавливаем относительный путь для корректной работы всех путей
   server: {
     port: 3000,
     strictPort: true,
     mimeTypes: {
-      'application/javascript': ['.js', '.jsx'], // Явно указываем MIME-типы
+      'application/javascript': ['.js', '.jsx'],
     },
   },
-});
+  
+  export default defineConfig({
+    base: '/',
+  });
